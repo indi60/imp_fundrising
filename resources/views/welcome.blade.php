24e60eeda7bd20bb34ae7bb7c8b2df1e9c45d5a7
BagasArbiansyah <bagasarbi89@gmail.com>
2019-12-03 16:30:07 +0700

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Bagas Arbiansyah
+                    Bagas Arbiansyah RPL
                 </div>
 
                 <div class="links">
